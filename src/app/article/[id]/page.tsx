104b475074f9291e24d470255b6c37b4674f0858
engjango <lavradodosilicio@gmail.com>
2025-04-11 10:05:20 -0400

    Added DynamicRoute

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -1,7 +1,8 @@
 // src/app/article/[id]/page.tsx
 
 import ArticleView from './ArticleView';
+import { type DynamicRoute } from 'next';
 
-export default function Page({ params }: { params: { id: string } } & { params: any }) {
+export default function Page({ params }: DynamicRoute<{ id: string }>) {
   return <ArticleView id={params.id} />;
 }
