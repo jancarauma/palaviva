9dff97fb91b3ac3dafaf8aae1d945d0832919294
engjango <lavradodosilicio@gmail.com>
2025-04-11 09:50:26 -0400

    Removed PageProps

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -1,11 +1,8 @@
-type PageProps = {
-  params: {
-    id: string;
-  };
-};
+// src/app/article/[id]/page.tsx
 
 import ArticleView from './ArticleView';
 
-export default function Page({ params }: PageProps) {
+export default function Page({ params }) {
+  // params.id aqui já vem corretamente tipado pelo Next.js
   return <ArticleView id={params.id} />;
 }
