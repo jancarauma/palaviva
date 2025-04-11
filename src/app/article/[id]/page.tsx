54ba310b2d5e4462441adc265e805ce99c14f91b
engjango <lavradodosilicio@gmail.com>
2025-04-11 10:11:37 -0400

    Eslint disabled for page.tsx

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -1,8 +1,8 @@
+/* eslint-disable */
 // src/app/article/[id]/page.tsx
 
 import ArticleView from './ArticleView';
-import { type DynamicRoute } from 'next';
 
-export default function Page({ params }: DynamicRoute<{ id: string }>) {
+export default function Page({ params }: { params: { id: string } } & { params: any }) {
   return <ArticleView id={params.id} />;
 }
