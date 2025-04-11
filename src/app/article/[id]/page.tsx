ad2fbf81a34da8e5663db3e8237aa023d54c36c9
engjango <lavradodosilicio@gmail.com>
2025-04-11 09:47:09 -0400

    PageProps added

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -1,5 +1,11 @@
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
+
 import ArticleView from './ArticleView';
 
-export default function Page({ params }: { params: { id: string } }) {
+export default function Page({ params }: PageProps) {
   return <ArticleView id={params.id} />;
 }
